ffb76cc49744ca7d66b82169876ddeeca2d8bfeb
Anshupriyaa <anshupriya05072004@gmail.com>
2025-01-08 13:20:49 +0530

    4th question solved

---
+++ areaOfCircle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class areaOfCircle{
+ public static void main(String[] args){
+ 
+ Scanner sc = new Scanner(System.in);
+ double radius = sc.nextDouble();
+  
+  double area = 3.14 *( radius*radius);
+ 
+  System.out.println("AREA OF A CIRCLE IS : "+ area);
+}
+}
