61ccf660bd748636aef891d8e0eeef758e6e2f89
Anshupriyaa <anshupriya05072004@gmail.com>
2025-01-09 11:31:07 +0530

    done

---
+++ Agee.java
@@ -0,0 +1,9 @@
+public class Agee{
+ public static void main(String[] args){
+  int currYear=2024;
+  int birthYear=2000;
+  int ageOfHarry=currYear-birthYear;
+  System.out.println("CURRENT AGE OF HARRY IS: "+ ageOfHarry);
+
+}
+}
